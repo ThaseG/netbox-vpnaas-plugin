97a849686b945ac42b2b1e112e69b337314cb9c7
Andrej Hyben <andrej@hyben.net>
2025-07-29 15:52:14 +0200

    Removed wip files

--- netbox_docker_plugin/models/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Models definitions"""
-
-from .host import Host
-from .image import Image
-from .volume import Volume
-from .network import Network
-from .container import Container
-from .registry import Registry
